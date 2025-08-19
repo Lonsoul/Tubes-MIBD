ed8facd4d7870a22331d3f8f5cd2ec0827c1fdac
Tobias Budiman <tobiasbudiman@Tobiass-MacBook-Pro.local>
2025-08-19 16:44:31 +0700

    tes

--- Connect.java
+++ Connect.java
@@ -1,32 +1,32 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+// import java.sql.Connection;
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
 
-public class Connect {
-    public static void main(String[] args) {
-        String dbURL = "jdbc:sqlserver://localhost;encrypt=true;trustServerCertificate=true;databaseName=TubesMIBD";
-        String user = "sa";
-        String password = "dockerStrongPwd123";
+// public class Connect {
+//     public static void main(String[] args) {
+//         String dbURL = "jdbc:sqlserver://localhost;encrypt=true;trustServerCertificate=true;databaseName=TubesMIBD";
+//         String user = "sa";
+//         String password = "dockerStrongPwd123";
 
-        Connection conn = null;
-        try {
-            // Register the JDBC driver (optional for newer versions)
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//         Connection conn = null;
+//         try {
+//             // Register the JDBC driver (optional for newer versions)
+//             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
-            // Open a connection
-            conn = DriverManager.getConnection(dbURL, user, password);
+//             // Open a connection
+//             conn = DriverManager.getConnection(dbURL, user, password);
 
-            System.out.println("Connected to the database successfully!");
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-}
+//             System.out.println("Connected to the database successfully!");
+//         } catch (SQLException | ClassNotFoundException e) {
+//             e.printStackTrace();
+//         } finally {
+//             if (conn != null) {
+//                 try {
+//                     conn.close();
+//                 } catch (SQLException e) {
+//                     e.printStackTrace();
+//                 }
+//             }
+//         }
+//     }
+// }
